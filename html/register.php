dd4a773806a31ca050071ed5238c49b20039957e
Jeremy Lord <jlord_2006@hotmail.com>
2015-03-07 16:06:25 -0800

    Register php script mostly working

--- html/register.php
+++ html/register.php
@@ -1,17 +1,28 @@
-<html>
-<body>
-<?php
-    $pswd = password_hash($_POST["password"], PASSWORD_BCRYPT);
-    $pswd_confirm = password_hash($_POST["password_confirm"], PASSWORD_BCRYPT);
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>PIGS Login</title>
+<link rel="stylesheet" type="text/css" href="view.css" media="all">
+<script type="text/javascript" src="view.js"></script>
 
-    if ($pswd == $pswd_confirm) 
+</head>
+<body id="main_body" >
+    <img id="top" src="top.png" alt="">
+    <div id="form_container">
+	
+<?php
+    $pswd = sha1($_POST["password"]);
+    $pswd_confirm = sha1($_POST["password_confirm"]);
+
+    if (strcmp($pswd, $pswd_confirm) !== 0) 
     {
-        echo "Hello, {$_POST['first_name']} {$_POST['last_name']}. You have successfully created an account";
-        // Store data in DB here
+        echo "Your password and confirmed password did not match.";
     }
     else
     {
-        echo "Your password and confirmed password did not match.";
+        echo "Hello, {$_POST['first_name']} {$_POST['last_name']}. You have successfully created an account";
+        // Store data in DB here
     }
 ?>
 </body>
